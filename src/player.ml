6510c3a4c795f490256aa81b0df57b9c440a4e49
Evan Chen <ec723@cornell.edu>
2023-03-16 16:16:26 -0400

    Implemented player.ml, Edited player.mli
    - implemented player_health, player_block, player_hand
    - created a helper function add_card_name that add the card id to a list
    - implemented a new function add_card
    - Edited specs for player.mli
    - added add_card

--- src/player.ml
+++ src/player.ml
@@ -1,15 +1,26 @@
-type t
-(**player*)
+open Card
+type t = {
+  name : string; 
+  health : int; 
+  energy : int;
+  block : int; 
+  hand : Card.t list;
+  deck : Card.t list; 
+}
 
-(**get player health*)
-let player_health (p : t) : int = raise (Failure "unimplemented player_health")
+(**[player_health] returns the player's current health*)
+let player_health (p : t) : int = p.health
 
-(**get palyer block*)
-let player_block (p : t) : int = raise (Failure "unimplemented player_block")
+(**[player_block] returns the player's current block*)
+let player_block (p : t) : int = p.block
 
-(**name of cards player is holding*)
-let player_hand (p : t) : string list =
-  raise (Failure "unimplemented player_hand")
+let add_card_name (card : Card.t) lst = card.id::lst   
 
-(**player energy*)
-let player_energy (p : t) : int = raise (Failure "unimplemented player_health")
+(**[player_hand] returns the player's current hand*)
+let player_hand (p : t) : string list = List.fold_right add_card_name p.hand []
+
+(**[player_energy] returns the player's current energy*)
+let player_energy (p : t) : int = p.energy
+
+(**[add_card] adds card [c] to the player's deck*)
+let add_card (c : Card.t) = {t with deck = deck @ [c]}  
--- src/player.mli
+++ src/player.mli
@@ -1,14 +1,17 @@
 type t
-(**player*)
+(** The abstract type of values representing player. *)
 
 val player_health: t -> int
-(**get player health*)
+(**[player_health] returns the player's current health*)
 
 val player_block: t -> int
-(**get palyer block*)
+(**[player_block] returns the player's current block*)
 
 val player_hand: t -> string list
-(**name of cards player is holding*)
+(**[player_hand] returns the player's current hand*)
 
 val player_energy: t -> int
-(**player energy*)
+(**[player_energy] returns the player's current energy*)
+
+val add_card: c -> t
+(**[add_card] adds card [c] to the player's deck*)
