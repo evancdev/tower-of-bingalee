e72fcd9c6280d962896adb9900d2d926c8a6a1e3
Your Name <mkl78@cornell.edu>
2023-03-18 19:52:28 -0400

    Implemented enemy.ml

--- src/enemy.ml
+++ src/enemy.ml
@@ -1,12 +1,14 @@
 open Yojson.Basic.Util
 
-type enemy = {
+exception UnknownEnemy of string
+
+type t = {
   name : string;
   health : int;
   block : int;
 }
 
-type t = { enemies : enemy list }
+(* type t = { enemies : enemy list } *)
 
 let enemy_of_json j =
   {
@@ -15,8 +17,24 @@ let enemy_of_json j =
     block = j |> member "block" |> to_int;
   }
 
-let all_enemies_of_json j =
-  { enemies = j |> member "enemies" |> to_list |> List.map enemy_of_json }
+let enemy_database =
+  let data_dir_prefix = "data" ^ Filename.dir_sep in
+  let enemy_json = Yojson.Basic.from_file (data_dir_prefix ^ "enemy.json") in
+  enemy_json |> member "enemies" |> to_list |> List.map enemy_of_json
 
-let enemy_health t = 1
-let enemy_block t = 1
+(* let enemy_list t = t.enemies *)
+
+let init_enemy (enemy_name : string) =
+  let found = List.find (fun e -> e.name = enemy_name) enemy_database in
+  match found with
+  | exception Not_found -> raise (UnknownEnemy enemy_name)
+  | e -> e
+
+(* let rec get_enemy enemies (enemy : string) = match enemies with | [] -> raise
+   (UnknownEnemy "Enemy does not exist.") | h :: t -> if h.name = enemy then h
+   else get_enemy t enemy *)
+
+let change_health t damage = { t with health = t.health - damage }
+let change_block t = raise (Failure "Unimplemented")
+let enemy_health t = t.health
+let enemy_block t = t.block
