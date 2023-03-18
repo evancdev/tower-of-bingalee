1bb8a61160aa6564a444c35f168a04850c47c52e
Your Name <mkl78@cornell.edu>
2023-03-18 19:00:06 -0400

    Implemented command.ml and added a test case

--- test/main.ml
+++ test/main.ml
@@ -11,9 +11,13 @@ let data_dir_prefix = "data" ^ Filename.dir_sep
 let card_json = Yojson.Basic.from_file (data_dir_prefix ^ "card.json")
 let enemy_json = Yojson.Basic.from_file (data_dir_prefix ^ "enemy.json")
 
+let parse_test (name : string) str (expected_output : command) : test =
+  name >:: fun _ -> assert_equal expected_output (parse str)
+
 (** (TODO) create a starting_deck for the player*)
 let card_tests = []
-let command_tests = []
+let command_tests = [parse_test "extra spaces with play" "play  omega attack  "
+(Play [ "omega"; "attack" ]);]
 let enemy_tests = []
 let player_tests = []
 let state_tests = []
