1834d2665a24bc191d1005ca31ed65c00a8bf9fe
Evan Chen <ec723@cornell.edu>
2023-03-18 18:47:01 -0400

    Fixed player.ml and player.mli

--- src/player.ml
+++ src/player.ml
@@ -4,8 +4,8 @@ type t = {
   health : int; 
   energy : int;
   block : int; 
-  hand : Card.t;
-  deck : Card.t; 
+  hand : card list;
+  deck : card list; 
 }
 
 (**[player_health] returns the player's current health*)
@@ -14,44 +14,13 @@ let player_health (p : t) : int = p.health
 (**[player_block] returns the player's current block*)
 let player_block (p : t) : int = p.block
 
-let add_card_name (card) lst = Card.description card::lst   
+let add_card_name (card:Card.card) lst = card::lst   
 
 (**[player_hand] returns the player's current hand*)
-let player_hand (p : t) = raise (Failure "unimplemented")
+let player_hand (p : t) = List.fold_right add_card_name p.hand []
 
 (**[player_energy] returns the player's current energy*)
 let player_energy (p : t) : int = p.energy
 
 (**[add_card] adds card [c] to the player's deck*)
-let add_card (p : t) (c : Card.t) = {p with deck = p.deck @ c}  
-=======
-open Card
-
-type t = {
-  name : string;
-  health : int;
-  energy : int;
-  block : int;
-  hand : Card.t list;
-  deck : Card.t list;
-}
-
-(**[player_health] returns the player's current health*)
-let player_health (p : t) : int = p.health
-
-(**[player_block] returns the player's current block*)
-let player_block (p : t) : int = p.block
-
-(**let add_card_name (card : Card.t) lst = card.id :: lst*)
-
-(**[player_hand] returns the player's current hand*)
-
-(**let player_hand (p : t) : string list = List.fold_right add_card_name p.hand
-   []*)
-
-(**[player_energy] returns the player's current energy*)
-let player_energy (p : t) : int = p.energy
-
-(**[add_card] adds card [c] to the player's deck*)
-
-(**let add_card (c : Card.t) = { t with deck = deck @ [ c ] }*)
+let add_card (p : t) (c : card) = {p with deck = p.deck @ [c]}  
