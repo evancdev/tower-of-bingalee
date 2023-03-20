fcd2efdbc9e6ac1229b2a0befe052d9a7a367de4
Evan Chen <ec723@cornell.edu>
2023-03-20 16:41:51 -0400

    Created a draw function in player.ml

--- src/player.ml
+++ src/player.ml
@@ -13,8 +13,8 @@ let create_player (name : string) =
   health = 50; 
   energy = 3;
   block = 0;
-  hand = [];
-  deck = ["OMEGA attack";"OMEGA attack";"OMEGA attack";"OMEGA attack"];
+  hand = ["OMEGA attack"];
+  deck = ["OMEGA attack";"OMEGA attack";"OMEGA attack"];
   }
 
 (**[player_health] returns the player's current health*)
@@ -26,11 +26,17 @@ let player_block (p : t) : int = p.block
 let add_card_name (card_id: string) lst : string list = card_id::lst   
 
 (**[player_hand] returns the player's current hand*)
-let player_hand (p : t) = List.fold_right add_card_name p.hand []
+let player_hand (p : t) : string list = List.fold_right add_card_name p.hand []
 
 (**[player_energy] returns the player's current energy*)
 let player_energy (p : t) : int = p.energy
 
 (**[add_card] adds [card_name] to the player's deck*)
-let add_card (p : t) (card_name : string) = {p with deck = p.deck @ [card_name]}  
+let add_card (p : t) (card_name : string) : t = {p with deck = p.deck @ [card_name]}  
 
+let updated_deck (deck : string list) = raise (Failure "unimplemented")
+
+let new_hand (hand : string list)= raise (Failure "unimplemented")
+
+(**[draw] updates the player's field whenever he draws*)
+let draw (p : t) : t = {p with deck = updated_deck p.deck; hand = new_hand p.hand}
