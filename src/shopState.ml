d62cc415a6c0f3ca925099af030f7437c63c7628
Evan <ec723@cornell.edu>
2023-04-25 16:21:55 -0400

    added two additional fields to type t and implemented a card removal service (buy_card_removal)

--- src/shopState.ml
+++ src/shopState.ml
@@ -4,13 +4,17 @@ open Random
 
 exception InvalidPurchase of string
 exception CannotSell of string
+exception CardRemoval of string
 
 type t = {
   cards : string list;
   gold : int;
   player : Player.t;
+  card_removal : int;
+  removal_cost : int;
 }
 
+let removal_price = ref 3
 let floor = 1
 let tier1 = []
 let tier2 = []
@@ -36,7 +40,14 @@ let create_shop (floor : int) (depth : int) (player : Player.t) =
     cards = generate_shop_cards floor;
     gold = (floor * 7) + (depth * 2);
     player;
+    card_removal = 1;
+    removal_cost = !removal_price;
   }
+
+let get_cards (shop : t) = shop.cards
+let get_gold (shop : t) = shop.gold
+let get_card_removals (shop : t) = shop.card_removal
+let get_removal_cost (shop : t) = shop.removal_cost
 
 let sell_card (player : Player.t) (shop : t) (card : string) =
   match List.mem card (player_cards player) with
@@ -44,13 +55,14 @@ let sell_card (player : Player.t) (shop : t) (card : string) =
       raise (CannotSell "ARGH! You cannot sell a card that you don't have!")
   | true ->
       {
+        shop with
         cards = card :: shop.cards;
         gold = shop.gold - 1;
         player = Player.remove_card player card;
       }
 
 (* duplicate code from player.ml, maybe find a way to remove duplicate code?*)
-let remove_card (shop : t) (card_name : string) : t =
+let shop_remove_card (shop : t) (card_name : string) : t =
   let rec removing (count : int) =
     match shop.cards with
     | [] -> []
@@ -68,9 +80,20 @@ let buy_card (player : Player.t) (shop : t) (card : string) =
       raise (InvalidPurchase "The shop isn't selling that card. Too bad!")
   | true ->
       {
-        (remove_card shop card) with
+        (shop_remove_card shop card) with
         gold = shop.gold + 1;
         player = change_gold_player (add_card player card) (-1);
       }
+
+let buy_card_removal (player : Player.t) (shop : t) (card : string) =
+  match shop.card_removal with
+  | 1 ->
+      removal_price := !removal_price + 2;
+      {
+        (shop_remove_card shop card) with
+        player = change_gold_player (remove_card player card) (- !removal_price);
+        removal_cost = !removal_price;
+      }
+  | _ -> raise (CardRemoval "The shop is out of card removals")
 
 let get_player_state (shop : t) = shop.player
--- src/shopState.mli
+++ src/shopState.mli
@@ -7,11 +7,26 @@ exception CannotSell of string
 val create_shop : int -> int -> Player.t -> t
 (**creates a shop based on the current floor and depth*)
 
+val get_cards : t -> string list
+(**gets the cards that the shop is selling*)
+
+val get_gold : t -> int
+(**gets the shop's gold*)
+
+val get_card_removals : t -> int
+(**gets the shop's card removal*)
+
+val get_removal_cost : t -> int
+(**gets the shop's card removal cost*)
+
 val sell_card : Player.t -> t -> string -> t
 (**the player sells a card to the shop*)
 
 val buy_card : Player.t -> t -> string -> t
 (**the player buys a card from the shop*)
 
+val buy_card_removal : Player.t -> t -> string -> t
+(**the player buys a card removal from the shop*)
+
 val get_player_state : t -> Player.t
-(**returns the player state*)
+(**gets the player state*)
